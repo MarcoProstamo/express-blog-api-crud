18d8a1ca1c38fad11ad02ac01a64bbd60b2cb730
MarcoProstamo <Marcopros1201@gmail.com>
2024-11-21 13:29:38 +0100

    Add → Filter on Index

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,13 @@
 const posts = require("../db/posts");
 
 function index(req, res) {
-  res.json(posts);
+  const filter = req.query.filter ?? "";
+  let validPosts = [];
+  const filteredPosts = posts.filter((post) => {
+    if (post.tags.includes(filter.toLowerCase())) validPosts.push(post);
+    return validPosts;
+  });
+  validPosts.length > 0 ? res.json(validPosts) : res.json(posts);
 }
 
 function show(req, res) {
