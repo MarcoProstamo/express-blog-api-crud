c7eea2f1616645091d4d71cdd50a589564af70e0
MarcoProstamo <Marcopros1201@gmail.com>
2024-11-21 12:58:42 +0100

    first commit

---
+++ db/posts.js
@@ -0,0 +1,39 @@
+const posts = [
+  {
+    id: 1,
+    titolo: "Post 1",
+    contenuto: "Lorem Ipsum",
+    immagine: "./images/ciambellone.jpeg",
+    tags: ["Sweet", "Soft", "Light"],
+  },
+  {
+    id: 12,
+    titolo: "Post 2",
+    contenuto: "Lorem Ipsum",
+    immagine: "./images/cracker_barbabietola.jpeg",
+    tags: ["Sweet", "Hard", "Light", "Small"],
+  },
+  {
+    id: 8,
+    titolo: "Post 3",
+    contenuto: "Lorem Ipsum",
+    immagine: "./images/pane_fritto_dolce.jpeg",
+    tags: ["Sweet", "Hard", "Heavy"],
+  },
+  {
+    id: 4,
+    titolo: "Post 4",
+    contenuto: "Lorem Ipsum",
+    immagine: "./images/pasta_barbavietola.jpeg",
+    tags: ["Heavy"],
+  },
+  {
+    id: 3,
+    titolo: "Post 5",
+    contenuto: "Lorem Ipsum",
+    immagine: "./images/torta_paesana.jpeg",
+    tags: ["Sweet", "Soft", "Heavy"],
+  },
+];
+
+module.exports = posts;
